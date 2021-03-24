e68e388879fd7950dd6eeba63407e24d2572b62b
Lucien <lucienrc@outlook.com.br>
2021-03-24 00:36:23 -0300

    test: create test to find parameters passed to the CardWidget

---
+++ test/presenter/pages/home/widgets/card_widget_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_example/presenter/pages/home/widgets/card_widget.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('CardWidget', (WidgetTester tester) async {
+    final icon = Icons.people;
+    final title = 'GitHub Users';
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CardWidget(
+            title: title,
+            icon: icon,
+          ),
+        ),
+      ),
+    );
+
+    final iconFinder = tester.widget<Icon>(
+      find.byKey(
+        Key('card-icon'),
+      ),
+    );
+    final titleFinder = tester.widget<Text>(
+      find.byKey(
+        Key('card-title'),
+      ),
+    );
+
+    expect(iconFinder.icon, equals(icon));
+    expect(titleFinder.data, equals(title));
+  });
+}
